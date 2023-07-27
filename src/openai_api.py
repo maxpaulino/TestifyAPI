8eaa09106fc44dbd8919a5b7987a55ecfe5cd044
Max Paulino <max.paulinor@gmail.com>
2023-07-27 16:38:45 -0600

    openai_api.py restructuring fix

--- src/openai_api.py
+++ src/openai_api.py
@@ -8,25 +8,53 @@ import openai
 
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
+def generate_true_false(tag, level): 
+
+    prompt = (
+  
+        "Generate one true or false question with an answer. This question should be a " + 
+        "Duolingo English Language question at the {} level and should be in relation to this topic: {}}. "
+
+    ).format(level, tag)
+
+    completion = openai.ChatCompletion.create(
+
+        model="gpt-4-0613",
+        messages=[
+            {"role": "system", "content": 
+
+            "You are a bot that exclusively generates true or false questions in this format:\n" +
+            "Q: The earth is a sphere \n\nA: True\n" + 
+            "Q: An elephant can fly \n\nA: False",
+            
+            },
+            {"role": "user", "content": prompt}
+        ],
+        temperature=0.75
+    )
+
+    return completion.choices[0].message.content
+
 def generate_mult_choice(tag, level): 
 
     prompt = (
 
-        "Generate a multiple-choice question with 4 options and the answer. "
+        "Generate one multiple-choice-question with 4 options and the answer. "
         "This question should be a CEFR certified English Language Exam question at the {} level and should be in relation to this topic: {}. "
 
     ).format(level, tag)
 
     completion = openai.ChatCompletion.create(
 
-        model="gpt-3.5-turbo",
+        model="gpt-4-0613",
         messages=[
             {"role": "system", "content": 
 
-                "You are a bot that exclusively generates multiple-choice questions in this format:\n" + 
-             "Q:\n\nA)\nB)\nC)\nD)\n\nA:"
+            "You are a bot that exclusively generates true or false questions in this format:\n" +
+            "Q: The earth is a sphere \n\nA: True\n" + 
+            "Q: An elephant can fly \n\nA: False",
 
-             },
+            },
             {"role": "user", "content": prompt}
         ],
         temperature=0.75
